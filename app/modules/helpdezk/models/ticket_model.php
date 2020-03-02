b96dd406c325763367ccab8932cb433df0087051
Rogerio Albandes <rogerio.albandes@helpdezk.cc>
2020-03-02 12:47:44 -0300

    Fix query

--- app/modules/helpdezk/models/ticket_model.php
+++ app/modules/helpdezk/models/ticket_model.php
@@ -1711,11 +1711,9 @@ class ticket_model extends DynamicTicket_model
 					LIMIT $start, $rp    
 				  ";
         }
-        //die($v_sql);
-        //print $v_sql ; exit;
-        //$ret = $this->db->Execute('SET CHARACTER SET utf8');
-        // $ret = $this->db->Execute($v_sql);
-        //$ret = $this->select($v_sql);
+
+        $ret = $this->db->Execute($v_sql);
+
         if (!$ret) {
             $sError = " Arq: " . __FILE__ . " Line: " . __LINE__ . "DB ERROR: " . $this->db->ErrorMsg() . " SQL: " . $v_sql;
             $this->error($sError);
