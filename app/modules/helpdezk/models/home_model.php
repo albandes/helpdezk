b7737383458888f18556a024d7575800560fe391
Rogerio Albandes <rogerio.albandes@gmail.com>
2021-03-26 15:25:54 -0300

    Fix operator without group issue

--- app/modules/helpdezk/models/home_model.php
+++ app/modules/helpdezk/models/home_model.php
@@ -22,197 +22,137 @@ class home_model extends DynamicHome_model
 
     public function getOperatorRequestStats($idPerson, $groupsId,$andQuery = "",$locale = "en_US")
     {
-        $sql =  "
-                SELECT 
-                  t1.requests,
-                  t2.in_atendance,
-                  t3.new_requests,
-                  t4.finished,
-                  t5.waiting_approval,
-                  t6.total_requests 
-                FROM
-                  (SELECT 
-                    FORMAT(COUNT(DISTINCT a.code_request),0,'{$locale}') AS requests      
-                  FROM
-                    (
-                      hdk_tbrequest a,
-                      hdk_tbstatus b,
-                      hdk_tbrequest_in_charge c,
-                      tbperson d,
-                      tbperson e
-                    ) 
-                  WHERE (
-                      (
-                        c.ind_in_charge = 1 
-                        AND c.id_in_charge IN ({$groupsId})
-                      ) 
-                      OR (
-                        c.ind_operator_aux = 1 
-                        AND c.id_in_charge = {$idPerson}
-                      ) 
-                      OR (
-                        c.id_in_charge IN ({$groupsId}) 
-                        AND c.ind_track = 1
-                      )
-                    ) 
-                    {$andQuery}
-                    AND c.id_in_charge = d.idperson 
-                    AND a.idstatus = b.idstatus 
-                    AND a.code_request = c.code_request 
-                    AND a.idperson_owner = e.idperson 
-                    AND b.idstatus = a.idstatus 
-                    AND b.idstatus_source <= 5) AS t1,
-                  (SELECT 
-                    COUNT(DISTINCT a.code_request) AS in_atendance 
-                  FROM
-                    (
-                      hdk_tbrequest a,
-                      hdk_tbstatus b,
-                      hdk_tbrequest_in_charge c,
-                      tbperson d,
-                      tbperson e
-                    ) 
-                  WHERE (
-                      (
-                        c.ind_in_charge = 1 
-                        AND c.id_in_charge IN ({$groupsId})
-                      ) 
-                      OR (
-                        c.ind_operator_aux = 1 
-                        AND c.id_in_charge = {$idPerson}
-                      ) 
-                      OR (
-                        c.id_in_charge IN ({$groupsId}) 
-                        AND c.ind_track = 1
-                      )
-                    ) 
-                    {$andQuery}
-                    AND c.id_in_charge = d.idperson 
-                    AND a.idstatus = b.idstatus 
-                    AND a.code_request = c.code_request 
-                    AND a.idperson_owner = e.idperson 
-                    AND b.idstatus = a.idstatus 
-                    AND b.idstatus_source = 3) AS t2,
-                  (SELECT 
-                    COUNT(DISTINCT a.code_request) AS new_requests 
-                  FROM
-                    (
-                      hdk_tbrequest a,
-                      hdk_tbstatus b,
-                      hdk_tbrequest_in_charge c,
-                      tbperson d,
-                      tbperson e
-                    ) 
-                  WHERE (
-                      (
-                        c.ind_in_charge = 1 
-                        AND c.id_in_charge IN ({$groupsId})
-                      ) 
-                      OR (
-                        c.ind_operator_aux = 1 
-                        AND c.id_in_charge = {$idPerson}
-                      ) 
-                      OR (
-                        c.id_in_charge IN ({$groupsId}) 
-                        AND c.ind_track = 1
-                      )
-                    ) 
-                    {$andQuery}
-                    AND c.id_in_charge = d.idperson 
-                    AND a.idstatus = b.idstatus 
-                    AND a.code_request = c.code_request 
-                    AND a.idperson_owner = e.idperson 
-                    AND b.idstatus = a.idstatus 
-                    AND b.idstatus_source = 1) AS t3,
-                  (SELECT 
-                    COUNT(DISTINCT a.code_request) AS finished 
-                  FROM
-                    (
-                      hdk_tbrequest a,
-                      hdk_tbstatus b,
-                      hdk_tbrequest_in_charge c,
-                      tbperson d,
-                      tbperson e
-                    ) 
-                  WHERE (
-                      (
-                        c.ind_in_charge = 1 
-                        AND c.id_in_charge IN ({$groupsId})
-                      ) 
-                      OR (
-                        c.ind_operator_aux = 1 
-                        AND c.id_in_charge = {$idPerson}
-                      ) 
-                      OR (
-                        c.id_in_charge IN ({$groupsId}) 
-                        AND c.ind_track = 1
-                      )
-                    ) 
-                    {$andQuery}
-                    AND c.id_in_charge = d.idperson 
-                    AND a.idstatus = b.idstatus 
-                    AND a.code_request = c.code_request 
-                    AND a.idperson_owner = e.idperson 
-                    AND b.idstatus = a.idstatus 
-                    AND b.idstatus_source = 5) AS t4,
-                  (SELECT 
-                    COUNT(DISTINCT a.code_request) AS waiting_approval 
-                  FROM
-                    (
-                      hdk_tbrequest a,
-                      hdk_tbstatus b,
-                      hdk_tbrequest_in_charge c,
-                      tbperson d,
-                      tbperson e
-                    ) 
-                  WHERE (
-                      (
-                        c.ind_in_charge = 1 
-                        AND c.id_in_charge IN ({$groupsId})
-                      ) 
-                      OR (
-                        c.ind_operator_aux = 1 
-                        AND c.id_in_charge = {$idPerson}
-                      ) 
-                      OR (
-                        c.id_in_charge IN ({$groupsId}) 
-                        AND c.ind_track = 1
-                      )
-                    ) 
-                    {$andQuery}
-                    AND c.id_in_charge = d.idperson 
-                    AND a.idstatus = b.idstatus 
-                    AND a.code_request = c.code_request 
-                    AND a.idperson_owner = e.idperson 
-                    AND b.idstatus = a.idstatus 
-                    AND b.idstatus_source = 4) AS t5,
-                  (SELECT 
-                    FORMAT(COUNT(DISTINCT a.code_request),0,'{$locale}') AS total_requests 
-                  FROM
-                    hdk_tbrequest a,
-                    hdk_tbstatus b,
-                    hdk_tbrequest_in_charge c 
-                  WHERE a.idstatus = b.idstatus 
-                    {$andQuery}
-                    AND c.ind_in_charge = 1 
-                    AND c.code_request = a.code_request 
-                    AND b.idstatus_source <= 5) AS t6         
-        
-                ";
-
-
-        $ret = $this->db->Execute($sql);
-
-        if ($this->db->ErrorNo() != 0) {
-            $this->dbError(__FILE__, __LINE__, __METHOD__, $this->db->ErrorMsg(), $sql);
-            return false ;
-        }
-
-        return $ret;
-
-
+      $condGroup = strlen($groupsId) > 0 
+        ? " (
+          (c.ind_in_charge = 1 AND c.id_in_charge IN ({$groupsId})) 
+          OR (c.ind_operator_aux = 1 AND c.id_in_charge = {$idPerson}) 
+          OR (c.id_in_charge IN ({$groupsId}) AND c.ind_track = 1)
+          ) " : " (c.ind_operator_aux = 1 AND c.id_in_charge = {$idPerson}) ";
+    
+      $sql =  "
+          SELECT 
+            t1.requests,
+            t2.in_atendance,
+            t3.new_requests,
+            t4.finished,
+            t5.waiting_approval,
+            t6.total_requests 
+          FROM
+            (SELECT 
+            FORMAT(COUNT(DISTINCT a.code_request),0,'{$locale}') AS requests      
+            FROM
+            (
+              hdk_tbrequest a,
+              hdk_tbstatus b,
+              hdk_tbrequest_in_charge c,
+              tbperson d,
+              tbperson e
+            ) 
+            WHERE {$condGroup}
+            {$andQuery}
+            AND c.id_in_charge = d.idperson 
+            AND a.idstatus = b.idstatus 
+            AND a.code_request = c.code_request 
+            AND a.idperson_owner = e.idperson 
+            AND b.idstatus = a.idstatus 
+            AND b.idstatus_source <= 5) AS t1,
+            (SELECT 
+            COUNT(DISTINCT a.code_request) AS in_atendance 
+            FROM
+            (
+              hdk_tbrequest a,
+              hdk_tbstatus b,
+              hdk_tbrequest_in_charge c,
+              tbperson d,
+              tbperson e
+            ) 
+            WHERE {$condGroup} 
+            {$andQuery}
+            AND c.id_in_charge = d.idperson 
+            AND a.idstatus = b.idstatus 
+            AND a.code_request = c.code_request 
+            AND a.idperson_owner = e.idperson 
+            AND b.idstatus = a.idstatus 
+            AND b.idstatus_source = 3) AS t2,
+            (SELECT 
+            COUNT(DISTINCT a.code_request) AS new_requests 
+            FROM
+            (
+              hdk_tbrequest a,
+              hdk_tbstatus b,
+              hdk_tbrequest_in_charge c,
+              tbperson d,
+              tbperson e
+            ) 
+            WHERE {$condGroup} 
+            {$andQuery}
+            AND c.id_in_charge = d.idperson 
+            AND a.idstatus = b.idstatus 
+            AND a.code_request = c.code_request 
+            AND a.idperson_owner = e.idperson 
+            AND b.idstatus = a.idstatus 
+            AND b.idstatus_source = 1) AS t3,
+            (SELECT 
+            COUNT(DISTINCT a.code_request) AS finished 
+            FROM
+            (
+              hdk_tbrequest a,
+              hdk_tbstatus b,
+              hdk_tbrequest_in_charge c,
+              tbperson d,
+              tbperson e
+            ) 
+            WHERE {$condGroup} 
+            {$andQuery}
+            AND c.id_in_charge = d.idperson 
+            AND a.idstatus = b.idstatus 
+            AND a.code_request = c.code_request 
+            AND a.idperson_owner = e.idperson 
+            AND b.idstatus = a.idstatus 
+            AND b.idstatus_source = 5) AS t4,
+            (SELECT 
+            COUNT(DISTINCT a.code_request) AS waiting_approval 
+            FROM
+            (
+              hdk_tbrequest a,
+              hdk_tbstatus b,
+              hdk_tbrequest_in_charge c,
+              tbperson d,
+              tbperson e
+            ) 
+            WHERE {$condGroup}
+            {$andQuery}
+            AND c.id_in_charge = d.idperson 
+            AND a.idstatus = b.idstatus 
+            AND a.code_request = c.code_request 
+            AND a.idperson_owner = e.idperson 
+            AND b.idstatus = a.idstatus 
+            AND b.idstatus_source = 4) AS t5,
+            (SELECT 
+            FORMAT(COUNT(DISTINCT a.code_request),0,'{$locale}') AS total_requests 
+            FROM
+            hdk_tbrequest a,
+            hdk_tbstatus b,
+            hdk_tbrequest_in_charge c 
+            WHERE a.idstatus = b.idstatus 
+            {$andQuery}
+            AND c.ind_in_charge = 1 
+            AND c.code_request = a.code_request 
+            AND b.idstatus_source <= 5) AS t6         
+      
+          ";
+    
+    
+      $ret = $this->db->Execute($sql);
+      
+      if ($this->db->ErrorNo() != 0) {
+        $this->dbError(__FILE__, __LINE__, __METHOD__, $this->db->ErrorMsg(), $sql);
+        return false ;
+      }
+    
+      return $ret;
     }
-
+    
     public function getUserRequestStats($idPerson, $andQuery = "",$locale = "en_US")
     {
         $sql =  "
