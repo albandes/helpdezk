be4dab77630ed0bbb5dfbb1a2cac05f73a9c571c
Valentín L Acosta <vilaxr@gmail.com>
2021-11-16 06:41:16 -0300

    add comments 121121

--- app/core/App.php
+++ app/core/App.php
@@ -4,8 +4,9 @@ namespace App\core;
 
 
 /**
-* Esta classe é responsável por obter da URL o controller, método (ação) e os parâmetros
-* e verificar a existência dos mesmo.
+* en_us This class is responsible for getting the controller, method (action) and parameters from the URL and verifying their existence.
+* 
+* pt_br Esta classe é responsável por obter da URL o controller, método (ação) e os parâmetros e verificar a existência dos mesmo.
 */
 class App
 {
@@ -28,7 +29,14 @@ class App
         // calls a method of a class passing the parameters
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
-    
+        
+    /**
+     * en_us Configure the URL to redirect
+     * 
+     * pt_br Configura a URL para redirecionar
+     *
+     * @return void
+     */
     private function setUrl()
     {
         $_GET['url'] = (isset($_GET['url']) ? $_GET['url'] : '/admin/');
@@ -50,7 +58,9 @@ class App
     }
     
     /**
-     * Este método pega as informações da URL (após o dominio do site) e retorna esses dados
+     * en_us This method takes the information from the URL (after the site's domain) and returns that data.
+     * 
+     * pt_br Este método pega as informações da URL (após o dominio do site) e retorna esses dados
      *
      *  @return array
      */
@@ -66,11 +76,15 @@ class App
     }
     
     /**
-     * Este método verifica se o array informado possui dados na posição 1 (controlador)
+     * en_us This method checks if the informed array has data in position 1 (controller) 
+     * if it exists, checks if there is a file with that name in the app/controllers directory 
+     * and instantiates an object contained in the file, otherwise the variable $page404 receives true.
+     * 
+     * pt_br Este método verifica se o array informado possui dados na posição 1 (controlador)
      * caso exista, verifica se existe um arquivo com aquele nome no diretório app/controllers
      * e instancia um objeto contido no arquivo, caso contrário a variável $page404 recebe true.
      *
-     *  @param  array  $url   Array contendo informações ou não do controlador, método e parâmetros
+     *  @param  array  $url   Array containing information or not about the controller, method and parameters.
      */
     private function setController($url)
     {
@@ -87,11 +101,13 @@ class App
     }
     
     /**
-     * Este método verifica se o array informado possui dados na posição 2 (método)
-     * caso exista, verifica se o método existe naquele determinado controlador
-     * e atribui a variável $method da classe.
+     * en_us This method checks if the informed array has data in position 2 (method) 
+     * if it exists, checks if the method exists in that particular controller and assigns the class variable $method.
+     * 
+     * pt_br Este método verifica se o array informado possui dados na posição 2 (método)
+     * caso exista, verifica se o método existe naquele determinado controlador e atribui a variável $method da classe.
      *
-     * @param  array  $url   Array contendo informações ou não do controlador, método e parâmetros
+     * @param  array  $url   Array containing information or not about the controller, method and parameters.
      */
     private function setMethod($url)
     {
@@ -107,11 +123,15 @@ class App
     }
     
     /**
-     * Este método verifica se o array informador possui a quantidade de elementos maior que 3
+     * en_us TThis method checks if the informer array has the number of elements greater than 3 
+     * ($url[1] is the controller and $url[2] the method/action to execute), if so, 
+     * the variable $params of class one is assigned new array from position 3 of $url
+     * 
+     * pt_br Este método verifica se o array informador possui a quantidade de elementos maior que 3
      * ($url[1] é o controller e $url[2] o método/ação a executar), caso seja, é atrbuido
      * a variável $params da classe um novo array a partir da posição 3 do $url
      *
-     * @param  array  $url   Array contendo informações ou não do controlador, método e parâmetros
+     * @param  array  $url   Array containing information or not about the controller, method and parameters.
      */
     private function setParams($url)
     {
