5f198540e881cbc1c0481a9530648e9317e6522e
Valentín L Acosta <vilaxr@gmail.com>
2021-11-19 06:03:28 -0300

    change of 181121 part 2
    add select 2 - replacing chosen

---
+++ app/modules/admin/views/js/createholiday.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    countdown.start(timesession);
+
+    /*
+     * Datepicker
+     */
+    $('.input-group.date').datepicker();
+
+    $('#company').select2();
+});
+
